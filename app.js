c210380079acbbbc0331ce9fbd78405a97cf943e
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-03-09 19:39:05 +0500

    half compelete

---
+++ app.js
@@ -0,0 +1,59 @@
+// Answer # 1
+
+// var first =  prompt("enter first name");
+// var second = prompt("enter last name");
+
+// var fullName = first+" "+second;
+
+// console.log(fullName)
+
+
+//  Answer # 2
+
+// var favMobilePhone = prompt("enter Favorite Mobile Phone")
+// var length = favMobilePhone.length;
+// console.log(length)
+
+//  Answer # 3
+
+// var first = "Pakistani";
+
+// var second = first.indexOf("n")
+
+// console.log(second)
+ 
+
+//  Answer # 4
+
+// var first = "Hello World";
+
+// var second = first.lastIndexOf("l")
+
+// console.log(second)
+
+
+//  Answer # 5
+// var last = "Pakistani";
+
+// var lindex = last.charAt(3)
+
+// console.log(lindex)
+
+// Answer # 6
+// var first = "faizan";
+// var second ="mohsin";
+
+// var fullName = first.concat(" ",second);
+
+// console.log(fullName)
+
+// Answer # 7
+
+// var a ="Hyderabad"
+// var res = a.replace("Hyder","islam")
+// console.log(res)
+
+// Answer # 8
+var a = "472";    
+var b = parseInt(a)
+console.log(b)
